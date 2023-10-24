db9f01d7d41b8107c5c957f3555fd8a10303f6cb
Wael Hajji <waelhajji.contact@gmail.com>
2023-10-24 08:23:41 +0100

    Week1Day1

---
+++ introToEs6.js
@@ -0,0 +1,120 @@
+//!-------------------------------Why JS ?
+
+//* Developers use JavaScript to build complex interactive websites and browser games, and to connect servers to websites and web applications. Because of this versatility, it's easy to see why this language is the most commonly used programming language in the world.
+
+
+
+
+
+
+
+
+//!-------------------------------Is JavaScript Used for Front-End or Back-End ?
+
+
+//* Javascript is a client-side language, which technically makes it a front-end language. But with the Node.js framework,
+//* JavaScript can also be used in back-end. Using JavaScript across the whole stack, for both front-end and back-end,
+//* is an excellent practice because it creates a cohesive application. When as much code as possible is all in the same language, it makes it easier to maintain, manage, update, and build with a single team.
+
+
+
+
+//!-------------------------------What is ECMAScript ?
+
+
+//* ECMAScript, also known as JavaScript, is a programming language adopted by the European Computer Manufacturer's Association
+//* as a standard for performing computations in Web applications. ECMAScript is the official client-side scripting language of VoiceXML.
+//* ECMAScript is a limited program//ming model for simple data manipulation.
+
+
+// Single threaded non-blocking language
+
+var x = 10
+
+function test() {
+    console.log("hello")
+}
+
+test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//!-------------------------------what is es6 ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//!-------------------------------What is the difference between JavaScript and ES6 ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//!-------------------------------Comparison between ES5 and ES6 ?
